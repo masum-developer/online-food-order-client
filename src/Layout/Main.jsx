86cc02727b0015b527758d7bdcb1613255c942a4
Masum Billah <fabrika786@gmail.com>
2023-10-21 22:37:12 +0600

    nav bar added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
